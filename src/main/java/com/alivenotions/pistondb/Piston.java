f27c74d4f09a19105b9a4b896b69d98e5999ced7
Bhavdeep Dhanjal <bhavdeep@hey.com>
2023-10-07 01:30:33 +0200

    WIP

--- src/main/java/com/alivenotions/pistondb/Piston.java
+++ src/main/java/com/alivenotions/pistondb/Piston.java
@@ -1,14 +1,18 @@
 package com.alivenotions.pistondb;
 
 import com.google.protobuf.ByteString;
+
+import java.io.File;
 import java.io.IOException;
 import java.util.Optional;
 
 public class Piston {
     KeyDir keyDir;
+    DataFile dataFile;
 
-    public static Piston open() {
+    public static Piston open() throws IOException {
         Piston db = new Piston();
+        db.dataFile = DataFile.create(new File("/tmp"));
 
         KeyDir newKeyDir = new KeyDir();
         db.keyDir = newKeyDir;
@@ -17,17 +21,15 @@ public class Piston {
     }
 
     public ByteString get(ByteString key) throws IOException {
-        Optional<ByteString> result = keyDir.get(key);
+        Optional<DirEntry> result = keyDir.get(key);
         if (result.isEmpty()) {
             return null;
         }
-        return result.get();
+        return ByteString.empty();
     }
 
-    public void put(ByteString key, ByteString value) {
-        // Write to file -> pos, fileId
-        // Create a DirEntry
-        // Write to keyDir
-        keyDir.put(key, value);
+    public void put(ByteString key, ByteString value) throws IOException {
+        DirEntry dirEntry = dataFile.write(key, value);
+        keyDir.put(key, dirEntry);
     }
 }
